c66ec1a4769b500513a3dd45c3e9af42eec0b5bc
Rachel Hamby <tnthreat33@gmail.com>
2023-04-04 19:51:39 -0400

    commit tree 450656c370c031ad3038736381e09289daa4619b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears  VALUES (1,'Mr.Chocolate', 2, 'M', 'brown', 'mild', TRUE);
+INSERT INTO bears  VALUES (2,'Rowdy', 6, 'M', 'black', 'mild', TRUE);
+INSERT INTO bears  VALUES (3,'Tabitha', 3, 'F', 'brown', 'mean', FALSE);
+INSERT INTO bears  VALUES (4,'Sergeant Brown', 2, 'M', 'brown', 'wild', TRUE);
+INSERT INTO bears  VALUES (5,'Melissa', 10, 'F', 'white', 'mild', FALSE);
+INSERT INTO bears  VALUES (6,'Grinch', 1, 'M', 'brown', 'wild', TRUE);
+INSERT INTO bears  VALUES (7,'Wendy', 7, 'F', 'black', 'good', TRUE);
+INSERT INTO bears  VALUES (8,NULL, 2, 'M', 'brown', 'mild', TRUE);
